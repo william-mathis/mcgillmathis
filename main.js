1eb1eb63dc9e7a7f74b980995828e028be213c93
Will Mathis <william.m.mathis@gmail.com>
2018-01-29 23:43:12 -0500

    started friend finder

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+var friends = ["http://clairemolloy.com/",
+"http://mollyemmanugent.com/",
+"https://www.alanapipe.com/",
+"http://brettdahlberg.com/",
+"http://sskerr.com/",
+"https://www.mallorymoench.com/",
+"http://www.joynotoma.com/",
+"https://gerardodelvalle.myportfolio.com/bio",
+];
+console.log(friends)
+var friendLen = friends.length;
+console.log(friendLen);
+// var friendGenerate() {
+// 	Math.floor(Math.random() * friendLen)
+// };
+
 $(document).ready(function () {
 	$("#textTab").on("click", function (e) {
 		e.preventDefault();
